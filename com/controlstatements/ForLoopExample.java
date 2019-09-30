cf72a5e5a92551053b9e1e2bf8fc055b3888dd8c
srs <srs@srs-ThinkPad-T470>
2019-09-30 18:27:07 +0530

    Control Statements

---
+++ com/controlstatements/ForLoopExample.java
@@ -0,0 +1,13 @@
+package com.controlstatements;
+
+public class ForLoopExample {
+	public static void main(String[] args) {
+	      
+		int sum = 0;
+		   
+	      for (int i = 1; i <= 1000; ++i) {
+	         sum += i;     
+	      }
+		   
+	      System.out.println("Sum = " + sum);
+	   }}
